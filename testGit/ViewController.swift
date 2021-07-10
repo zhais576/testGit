122b136a767f48e4255bfc185ec00e16e5ffb69e
Jack Zhai <zhais@wustl.edu>
2021-07-10 16:51:07 -0700

    Testing push function

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func test1(){
+        print("testing push to remote")
+    }
 
 }
 
